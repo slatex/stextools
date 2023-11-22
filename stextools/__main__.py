77fe7b0c6c3333d5964898861fb79d8db7ddbd34
jfschaefer <jan.frederik.schaefer@fau.de>
2023-11-22 11:39:22 +0100

    document cli better

--- stextools/__main__.py
+++ stextools/__main__.py
@@ -8,7 +8,8 @@ logger = logging.getLogger(__name__)
 
 @click.group()
 @click.option('--keep-cache', is_flag=True, default=False,
-              help='Keep the cache despite changes to the stextools package (saves time when developing)')
+              help='Keep the cache despite changes to the stextools package.'
+                   'This can be useful for stextools development, but may lead to errors.')
 def cli(keep_cache):
     if keep_cache:
         Cache.clear = lambda: None  # type: ignore
@@ -16,13 +17,14 @@ def cli(keep_cache):
     logging.basicConfig(level=logging.INFO)
 
 
-@cli.command()
+@cli.command(help='Clear the cache. You should not have to do this '
+                  'as the cache is automatically cleared whenever stextools is updated.')
 def clear_cache():
     Cache.clear()
     logger.info('Cleared cache.')
 
 
-@cli.command()
+@cli.command(help='Update the archive dependencies.')
 @click.option('--mode', default='test',
               type=click.Choice(['test', 'ask', 'write'], case_sensitive=False),
               help='test: only print the changes, ask: ask before writing, write: write the changes without asking')
@@ -31,15 +33,15 @@ def update_dependencies(mode):
     dependency_check(mode)
 
 
-@cli.command()
+@cli.command(help='Visualizes the archive dependency graph (requires networkx and matplotlib).')
 @click.option('--filter', default=None,
               help='Filter pattern to only show some archives (e.g. \'smglom/*,MiKoMH/*\')')
-def show_graph(filter):
+def show_dependency_graph(filter):
     from stextools.dependency_graph import show_graph
     show_graph(filter)
 
 
-@cli.command()
+@cli.command(help='Lists archive dependencies that may be candidates for removal.')
 @click.option('--filter', default=None,
               help='Filter pattern to only show some archives (e.g. \'smglom/*,MiKoMH/*\')')
 def show_weak_dependencies(filter):
