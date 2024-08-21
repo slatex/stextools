e37e383b8d526854cbd3b2e4899c7b07c66db9b4
jfschaefer <jan.frederik.schaefer@fau.de>
2024-08-21 13:48:57 +0200

    work-around for pagination

--- stextools/remote_repositories.py
+++ stextools/remote_repositories.py
@@ -18,20 +18,20 @@ gl: gitlab.Gitlab = gitlab.Gitlab(URL)
 def clone_group(group: Group | str, recurse: bool = True):
     if isinstance(group, str):
         group = gl.groups.get(group)
-    projects = group.projects.list()
+    # TODO: deal properly with pagination (also below for subgroups)
+    projects = group.projects.list(per_page=1000)
 
-    directory = get_mathhub_path() / group.name
-    if not directory.exists():
-        directory.mkdir(parents=True)
 
     for project in projects:
-        if (directory / project.name).exists():
+        directory = get_mathhub_path() / project.path_with_namespace
+        if not directory.parent.exists():
+            directory.parent.mkdir(parents=True)
+        if directory.exists():
             print(f'Skipping {project.name} (already exists)')
             continue
-        print(f'Cloning {project.ssh_url_to_repo}')
-        subprocess.run(['git', 'clone', project.ssh_url_to_repo], check=True, cwd=directory)
+        subprocess.run(['git', 'clone', project.ssh_url_to_repo], check=True, cwd=directory.parent)
 
     if recurse:
-        groups = group.subgroups.list()
+        groups = group.subgroups.list(per_page=1000)
         for subgroup in groups:
             clone_group(gl.groups.get(subgroup.full_name.replace(' / ', '/')), recurse)
