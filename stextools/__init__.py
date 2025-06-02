6ea96f980579360bd7879ce99e25d9057d88158d
jfschaefer <jan.frederik.schaefer@fau.de>
2025-06-02 11:05:47 +0200

    simple Python api for symbol selection

---
+++ stextools/__init__.py
@@ -0,0 +1,6 @@
+from stextools.pythonapi import setup_logging, interactive_symbol_search
+
+__all__ = [
+    'setup_logging',
+    'interactive_symbol_search',
+]
---
+++ stextools/pythonapi.py
@@ -0,0 +1,62 @@
+import functools
+import logging
+from typing import Optional
+
+from stextools.core.cache import Cache
+from stextools.core.linker import Linker
+from stextools.core.simple_api import get_linker
+from stextools.core.stexdoc import Symbol
+from stextools.snify.selection import VerbTrie
+from stextools.snify.stemming import string_to_stemmed_word_sequence_simplified
+
+
+@functools.cache
+def get_linker() -> Linker:
+    mh = Cache.get_mathhub(update_all=True)
+    return Linker(mh)
+
+
+@functools.cache
+def get_verb_trie(lang: str) -> VerbTrie:
+    return VerbTrie(lang, get_linker())
+
+
+def setup_logging():
+    logging.getLogger('pylatexenc.latexwalker').setLevel(logging.WARNING)
+    # TODO: the linker indicates both real sTeX issues and missing features – we should not suppress them in general
+    logging.getLogger('stextools.core.linker').setLevel(logging.FATAL)
+    logging.basicConfig(level=logging.INFO)
+
+
+def serialize_symbol(symbol: Symbol) -> str:
+    symb_path = symbol.path_rel_to_archive
+    archive = symbol.declaring_file.archive.name
+    return f'[{archive}]{symb_path}'
+
+def interactive_symbol_search(verbalization: str, lang: str = 'en') -> Optional[str]:
+    vt = get_verb_trie(lang)
+    stemmed = string_to_stemmed_word_sequence_simplified(verbalization, lang)
+
+    subtrie = ([], vt.trie)
+    for word in stemmed:
+        if word not in subtrie[1]:
+            return None
+        subtrie = subtrie[1][word]
+
+    options: list[Symbol] = subtrie[0]
+
+    if not options:
+        return None
+
+    for i, symbol in enumerate(options):
+        print(f'[{i}]', serialize_symbol(symbol))
+
+    while True:
+        command: str = input('Command (number to select, "q" to quit): ').strip()
+        if command.isnumeric() and (i := int(command)) in range(len(options)):
+            return serialize_symbol(options[i])
+        elif command == 'q':
+            return None
+        else:
+            print(f'Invalid command: {command}. Please enter a number between 0 and {len(options) - 1} or "q" to quit.')
+            continue
