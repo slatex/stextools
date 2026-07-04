4ea511d9fb7e3dc3ca4681a92721d9c0c37a2260
Pouemogne-Ivana <ivana.pouemogne@fau.de>
2026-07-04 19:57:56 +0200

    symdecl

--- stextools/snify/verbalization_anno/verbalization_anno_type.py
+++ stextools/snify/verbalization_anno/verbalization_anno_type.py
@@ -32,7 +32,7 @@ class VerbalizationAnnoState:
 
 
 class AddVerbalizationCommand(Command):
-    def __init__(self, position: int, symbol_name:str, document_content:str, uri:str, num_args: int, suggestions):
+    def __init__(self, position: int, symbol_name:str, document_content:str, uri:str, num_args: int, suggestions ):
         self.position = position
         self.symbol_name= symbol_name
         self.document_content = document_content
@@ -47,7 +47,7 @@ class AddVerbalizationCommand(Command):
             description_long='Add a verbalization for the current \\symdef.'
         ))
 
-    def execute(self, call: str) -> list[CommandOutcome]:
+    def execute(self,  call: str) -> list[CommandOutcome]:
         """ this is called when the user presses 'a' """
         
         catalog = get_stex_catalogs()['en']  # english catalog
@@ -57,7 +57,42 @@ class AddVerbalizationCommand(Command):
                 for verbalization in catalog.symb_to_verb[symbol]:    
                     if verbalization.verb not in correspondances:
                         correspondances.append(verbalization.verb.replace('\n', ' '))      
-        suggestions=[]   
+        #print (correspondances)
+        suggestions=[] 
+        prep=" "
+        symdef_pos= self.document_content.rfind(r'\symdef', 0, self.position)
+        symdecl_pos= self.document_content.rfind(r'\symdecl', 0 , self.position)
+        if (symdef_pos != -1 and symdef_pos>symdecl_pos):
+            line = self.document_content[symdef_pos:].splitlines()[0]
+            #print(line)
+            match = re.search(r'\\symdef\{[^}]+\}(?:\[[^\]]*\])?\{(.*)\}$', line)
+
+            if match: #to control the positions of the arguments
+                form=match.group(1)
+                #print (form)
+            
+            match1= re.search(r'name=([^,\]]+)', line) #to add prepositions when there is a name in the symdef
+            if match1:
+                name = match1.group(1)
+                #print(name)
+                tokens_name=nltk.word_tokenize(name)
+                tags_name=nltk.pos_tag(tokens_name)
+           
+            else:
+                prepositions = ["for", "from", "at", "of", "to", "with"]
+                match2 = re.search(r'\\sym(?:def|decl)\*?\{([^}]*)\}', line)
+                sym_name= match2.group(1)
+                print(sym_name)
+                for p in prepositions:
+                    if sym_name.endswith(p):
+                        prep=p
+                        #prefix = sym_name[:-len(p)]
+                        #print(prefix, prep)
+                        break
+                
+        elif (symdecl_pos!=1 and symdecl_pos>symdef_pos):
+            line= self.document_content[symdecl_pos:].splitlines()[0]
+            
         
         for correspondance in correspondances:
             tokens = nltk.word_tokenize(correspondance)
@@ -77,18 +112,61 @@ class AddVerbalizationCommand(Command):
                 
            
             suggestion=""
-            if self.num_args==1:
-                for word,tag in converted:
-                    suggestion += f'{word }{tag} '
-                suggestion += "#1"
-                suggestions.append(suggestion.strip())
 
-            elif self.num_args==2:   
-                suggestion +="#1 "
-                for word,tag in converted:
-                    suggestion += f'{word }{tag} '
-                suggestion += "#2"
-                suggestions.append(suggestion.strip())
+            if self.num_args==1:
+                #pos= form.find('{#1}')
+                if (form.startswith('{#1}') or form.startswith('#1')):
+                    suggestion += "#1"
+                    for word,tag in converted:
+                        suggestion += f' {word }{tag}'       
+                    suggestions.append(suggestion.strip())
+                elif (form.endswith('{#1}') or form.endswith('#1')):
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f'{word} '
+                    else:
+                        suggestion+= " " +prep+ " "
+                    suggestion+="#1"
+                    suggestions.append(suggestion.strip())
+
+            elif self.num_args==2: 
+                positions1=[]
+                positions2=[]
+
+                positions1.append(form.find('{#1}'))
+                positions1.append(form.find('#1'))
+                positions2.append(form.find('{#2}'))
+                positions2.append(form.find('#2'))
+                pos1= max(positions1)
+                pos2= max(positions2)
+                if pos1 !=-1 and pos2!= -1 and pos1<pos2:
+                    suggestion +="#1 "
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f' {word}'
+                    else:
+                        suggestion+=  " " +prep + " "
+                    suggestion += " #2"
+                    suggestions.append(suggestion.strip())
+
+                elif pos1>pos2 and pos1!=-1:
+                    suggestion +="#2 "
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f' {word}'
+                    else:
+                        suggestion+= " " +prep+ " "
+                    suggestion += " #1"
+                    suggestions.append(suggestion.strip())
 
             elif self.num_args==3:
                 suggestion +="#1 "
@@ -136,7 +214,7 @@ class AddVerbalizationCommand(Command):
             interface.write_text("No annotation tag found.\n")
             return []
         letter=  specifications[pos[-1]+1]
-        if num_arguments== 1:
+        if ((num_arguments== 1) or (num_arguments== 0)):
             annotation_type= letter.upper()
         else:
             annotation_type= letter.upper() + str(num_arguments)
@@ -153,6 +231,8 @@ class AddVerbalizationCommand(Command):
 
         args=""
         while True:
+            if num_arguments==0:
+                break
             interface.write_text ('\nDo you need to enter the type of each argument c/d? just continue ("Enter") if not or enter the arguments:  ')
             entered_args= interface.get_input().strip()
             if entered_args =="":
@@ -180,6 +260,8 @@ class AddVerbalizationCommand(Command):
             ]
         
 #  python -m stextools snify --mode=text,verbalizations "C:\Users\ivana\Desktop\MathHub\smglom\algebra\source\mod\divgroup.en.tex"
+
+#  python -m stextools snify --mode=text,verbalizations "C:\Users\ivana\Desktop\MathHub\ai-agents\source\mod\goal-based-agent.en.tex"
 class DeleteVerbalizationCommand(Command):
     def __init__(self, position: int, symbol_name:str, document_content:str):
         self.position = position
@@ -259,11 +341,18 @@ class VerbalizationAnnoType(AnnoType[VerbalizationAnnoState]):
         # we only care about stuff after the current position
         document_content = document_content[position:]
 
-        our_position = document_content.find('\\symdef')
-        if our_position == -1:    # we did not find anything
+        positions = [p for p in (document_content.find('\\symdef'), document_content.find('\\symdecl')) if p!=-1]
+        if not positions: 
             return None
-        else:
-            return our_position + position, []
+        our_position = min(positions)
+        #if our_position == -1:    # we did not find anything
+            #return None
+        #elif(document_content.find('\\symdef')==-1 and document_content.find('\\symdecl')>=0 ):
+         #   our_position = document_content.find('\\symdecl')
+        #elif(document_content.find('\\symdef')>=0 and document_content.find('\\symdecl')==-1 ):
+         #   our_position = document_content.find('\\symdef')
+            
+        return our_position + position, []
 
     def show_current_state(self):
         interface.clear()
@@ -274,24 +363,34 @@ class VerbalizationAnnoType(AnnoType[VerbalizationAnnoState]):
 
         string = document_content[position:]
         line = string.splitlines()[0]
-
-        interface.write_text('\nCurrent \\symdef:\n\n')
-        interface.show_code(line, format='sTeX')
-
+        if line.startswith('\\symdef'):
+            interface.write_text('\nCurrent \\symdef:\n\n')
+            interface.show_code(line, format='sTeX')
+        else:
+            interface.write_text('\nCurrent \\symdecl:\n\n')
+            interface.show_code(line, format='sTeX')
 
         line_no = document_content[:position + 1].count('\n')
         uri = None
 
         annotations = FLAMS.get_file_annotations(str(self.snify_state.get_current_document().path), load=True)
         for e in json_iter(annotations):
-            if isinstance(e, dict) and 'Symdef' in e:
-                e_line_no = e['Symdef']['uri']['range']['start']['line']
+            if isinstance(e, dict) and line.startswith('\\symdef') and 'Symdef' in e:
+                
+                    e_line_no = e['Symdef']['uri']['range']['start']['line'] 
                 #print('COMPARING', line_no, e_line_no)
-                if line_no == e_line_no:
-                    uri = e['Symdef']['uri']['uri']
-                    break
-        symbol_name = uri.split('s=')[-1] if (uri and 's=') else 'UNKNOWN'
+                    if line_no == e_line_no:
+                        uri = e['Symdef']['uri']['uri']
+                        break
 
+            elif isinstance(e, dict) and line.startswith('\\symdecl') and 'Symdecl' in e:
+                
+                    e_line_no= e['Symdecl']['uri']['range']['start']['line'] 
+                    if line_no == e_line_no:
+                        uri = e['Symdecl']['uri']['uri']
+                        break
+        symbol_name = uri.split("s=")[-1] if (uri and 's=') else 'UNKNOWN'
+    
         pattern = rf'\\verbalization\{{{re.escape(symbol_name)}\}}\[.*?\]\{{.*?\}}\{{.*?\}}'
         matches = list (re.finditer(pattern, document_content))
         if not matches:
@@ -314,22 +413,35 @@ class VerbalizationAnnoType(AnnoType[VerbalizationAnnoState]):
 
         num_args=0
         line= string.splitlines()[0]
+        #print (line)
         match = re.search('args=(\\d+)', line)
         if match:
             num_args= int( match.group(1))
+        matche = re.search(r'\{([^{}]*)\}$', line)
+        if matche: 
+            content= matche.group(1)
+            #print (content)
 
         line_no = document_content[:position + 1].count('\n')
         uri = None
 
         annotations = FLAMS.get_file_annotations(str(self.snify_state.get_current_document().path), load=True)
         for e in json_iter(annotations):
-            if isinstance(e, dict) and 'Symdef' in e:
-                e_line_no = e['Symdef']['uri']['range']['start']['line']
+            if isinstance(e, dict) :
+                if line.startswith('\\symdef') and 'Symdef' in e:
+                    e_line_no = e['Symdef']['uri']['range']['start']['line'] 
                 #print('COMPARING', line_no, e_line_no)
-                if line_no == e_line_no:
-                    uri = e['Symdef']['uri']['uri']
-                    break
-        symbol_name = uri.split('s=')[-1] if (uri and 's=') else 'UNKNOWN'
+                    if line_no == e_line_no:
+                        uri = e['Symdef']['uri']['uri']
+                        break
+                    #symbol_name = uri.split("s=")[-1] if (uri and 's=') else 'UNKNOWN'
+                elif line.startswith('\\symdecl') and 'Symdecl' in e:
+                    e_line_no= e['Symdecl']['uri']['range']['start']['line'] 
+                    if line_no == e_line_no:
+                        uri = e['Symdecl']['uri']['uri']
+                        break
+        symbol_name = uri.split("s=")[-1] if (uri and 's=') else 'UNKNOWN'
+        
         catalog = get_stex_catalogs()['en']  # english catalog
         correspondances=[]
         for symbol in catalog.symb_iter():
@@ -339,7 +451,39 @@ class VerbalizationAnnoType(AnnoType[VerbalizationAnnoState]):
                         correspondances.append(verbalization.verb.replace('\n', ' '))      
            
         suggestions=[]   
+        prep = ""
+
         
+
+        if line.startswith('\\symdef'):
+            match = re.search(r'\\symdef\{[^}]+\}(?:\[[^\]]*\])?\{(.*)\}$', line)
+
+            if match:
+                form=match.group(1)
+                #print (form)
+            
+            #to add preposition on the suggestions because the suggestions from catalog generally doesn't have prepositions  
+            match1= re.search(r'name=([^,\]]+)', line)
+            if match1:
+                name = match1.group(1)
+                #print(name)
+                tokens_name=nltk.word_tokenize(name)
+                tags_name=nltk.pos_tag(tokens_name)
+            #print(tags_name)
+
+            else: #for the case that there is not a full name of the symbol in the sym
+                prepositions = ["for", "from", "at", "of", "to", "with"]
+                match2 = re.search(r'\\sym(?:def|decl)\*?\{([^}]*)\}', line)
+                name= match2.group(1)
+                #print(name)
+                for p in prepositions:
+                    if name.endswith(p):
+                        prep=p
+                        #prefix = name[:-len(p)]
+                        #print(prefix, prep)
+                        break
+        
+
         for correspondance in correspondances:
             tokens = nltk.word_tokenize(correspondance)
             tags = nltk.pos_tag(tokens)
@@ -358,18 +502,67 @@ class VerbalizationAnnoType(AnnoType[VerbalizationAnnoState]):
                 
         
             suggestion=""
+            
             if num_args==1:
-                for word,tag in converted:
-                    suggestion += f'{word }{tag} '
-                suggestion += "#1"
-                suggestions.append(suggestion)
+                #pos= form.find('{#1}')
+                if (form.startswith('{#1}') or form.startswith('#1')):
+                    suggestion += "#1"
+                    for word,tag in converted:
+                        suggestion += f' {word }{tag}'
+                    #add a preposition
+                   # for word, pos_name in tags_name:
+                     #   if pos_name in ('TO', 'IN'): 
+                            #suggestion+= f' {word}'
+                    suggestions.append(suggestion.strip())
 
-            elif num_args==2:   
-                suggestion +="#1 "
-                for word,tag in converted:
-                    suggestion += f'{word }{tag} '
-                suggestion += "#2"
-                suggestions.append(suggestion.strip())
+                elif (form.endswith('{#1}') or form.endswith('#1')):
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    #add a preposition
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f'{word} '
+                    elif prep:
+                        suggestion+= " " +prep+ " "
+                    suggestion+="#1\n"
+                    suggestions.append(suggestion.strip())
+
+            elif num_args==2: 
+                positions1=[]
+                positions2=[]
+
+                positions1.append(form.find('{#1}'))
+                positions1.append(form.find('#1'))
+                positions2.append(form.find('{#2}'))
+                positions2.append(form.find('#2'))
+                pos1= max(positions1)
+                pos2= max(positions2)
+                if pos1 !=-1 and pos2!= -1 and pos1<pos2:
+                    suggestion +="#1 "
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f'{word} '
+                    elif prep:
+                        suggestion+= " " +prep+ " "
+                    suggestion += "#2"
+                    suggestions.append(suggestion.strip())
+
+                elif pos1>pos2 and pos1!=-1:
+                    suggestion +="#2 "
+                    for word,tag in converted:
+                        suggestion += f'{word }{tag} '
+                    if match1:
+                        for word, pos_name in tags_name:
+                            if pos_name in ('TO', 'IN'): 
+                                suggestion+= f'{word} '
+                    elif prep:
+                        suggestion+= " " +prep+ " "   
+                    suggestion += "#1"
+                    suggestions.append(suggestion.strip())
 
             elif num_args==3:
                 suggestion +="#1 "
